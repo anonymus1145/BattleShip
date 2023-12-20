3eee8a6eee6ea7516300487f5e56026165576b5e
Anonymus1145 <Anonymus1145@yahoo.com>
2023-12-20 08:58:02 +0200

    Finish gameboard Unit Tests

--- src/_tests_/gameboard.test.ts
+++ src/_tests_/gameboard.test.ts
@@ -8,9 +8,27 @@ let gameboard = createGameboard();
 let firstShip = ship(5);
 
 
-describe('Gameboard', () => {
+describe('Gameboard function', () => {
     it('placeShip, should place a ship', () => {
         gameboard.placeShip(firstShip, 1, 1, true);
         expect(gameboard.ships.length).toBe(1);
+    })
+
+    it('receiveAttack, should record an missed attack', () => {
+        gameboard.receiveAttack(9 , 9);
+        expect(gameboard.missedAttacks.length).toBe(1);
+    })
+
+    it('receiveAttack, should record a hit', () => {
+        gameboard.receiveAttack(1 , 1);
+        gameboard.receiveAttack(2 , 1);
+        gameboard.receiveAttack(3 , 1);
+        gameboard.receiveAttack(4 , 1);
+        gameboard.receiveAttack(5 , 1);
+        expect(firstShip.hits).toBe(5);
+    })
+
+    it('areAllShipsSunk, should check if all ships are sunk', () => {
+        expect(firstShip.isSunk).toBe(true);
     })
 })
